8164a6ad31dcaeec358778ef7b68f1e1ec251db4
MusinxFate <murilodesouzabarros@gmail.com>
2021-12-01 01:41:28 -0300

    +Moviments (WASD + Jump) + Sprite //TODO: *Fix foward/backward moviments, fix sprite to be animated

--- Player.gd
+++ Player.gd
@@ -1,26 +1,52 @@
 extends KinematicBody
+var isRunning : bool = false
+var curHp : int = 10
+var maxHp : int = 10
+var damage : int = 1
+var gold : int = 0
+var attackRate : float = 0.3
+var lastAttackTime : int = 0
+var moveSpeed : float = 5.0
+var jumpForce : float = 10.0
+var gravity : float = 15.0
+var vel : Vector3 = Vector3()
+onready var camera = get_node("CameraOrbit")
 
-const FLOOR_NORMAL = Vector3(0.0, 1.0, 0.0)
+# called every physics step (60 times a second)
+func _physics_process(delta):  
+  vel.x = 0
+  vel.z = 0
+  
+  var input = Vector3()
+  
+  # movement inputs
+  if Input.is_action_pressed("up"): input.z -= 1
+  if Input.is_action_pressed("down"): input.z += 1
+  if Input.is_action_pressed("left"): input.x -= 1
+  if Input.is_action_pressed("right"): input.x += 1
 
-var speed := 7.0
-var gravity := 30.0
-var jump_force := 12.0
+  if Input.is_action_just_pressed("boost"): isRunning = true
+  if Input.is_action_just_released("boost"): isRunning = false
+  
+  # normalize the input vector to prevent increased diagonal speed
+  input = input.normalized()
+  
+  # get the relative direction
+  var dir = (transform.basis.z * input.z + transform.basis.x * input.x)
+  
+  # apply the direction to our velocity
+  if !isRunning:
+   vel.x = dir.x * moveSpeed
+   vel.z = dir.z * moveSpeed
 
-var velocity_y := 0.0
-
-func _physics_process(delta: float) -> void:
-	var direction_ground := Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")).normalized()
+  if isRunning:
+   vel.x = dir.x * moveSpeed * 5
+   vel.z = dir.z * moveSpeed * 5
+  
+  # gravity
+  vel.y -= gravity * delta
+  
+  if Input.is_action_pressed("jump") and is_on_floor(): vel.y = jumpForce
 	
-	if not is_on_floor():
-		velocity_y -= gravity * delta
-	
-	var velocity = Vector3(
-		direction_ground.x * speed,
-		velocity_y,
-		direction_ground.y * speed)
-	move_and_slide(velocity, FLOOR_NORMAL)
-	
-	if is_on_floor() or is_on_ceiling():
-		velocity_y = 0.0
+  # move along the current velocity
+  vel = move_and_slide(vel, Vector3.UP)
