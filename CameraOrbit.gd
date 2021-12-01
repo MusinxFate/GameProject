def690db8ac7873165ad99ac3c633e6584f2d1dd
MusinxFate <murilodesouzabarros@gmail.com>
2021-11-30 23:41:42 -0300

    Added camera moviments

---
+++ CameraOrbit.gd
@@ -0,0 +1,23 @@
+extends Spatial
+
+var lookSensitivity : float = 15.0
+var minLookAngle : float = -20.0
+var maxLookAngle : float = 75.0
+var mouseDelta : Vector2 = Vector2()
+onready var player = get_parent()
+
+func _input(event):  
+  if event is InputEventMouseMotion: mouseDelta = event.relative
+
+
+func _process(delta):  
+  var rot = Vector3(mouseDelta.y, mouseDelta.x, 0) * lookSensitivity * delta
+  rotation_degrees.x -= rot.x
+  rotation_degrees.x = clamp(rotation_degrees.x, minLookAngle, maxLookAngle)
+  
+  player.rotation_degrees.y -= rot.y
+  
+  mouseDelta = Vector2()
+
+func _ready():
+  Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
